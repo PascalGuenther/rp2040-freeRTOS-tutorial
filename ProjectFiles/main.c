2a538d9d0425564dce5f1090fecfaa5d8417958c
Pascal Guenther <pascal.develop@pguenther.net>
2022-06-18 04:26:19 +0200

    Part 2: Queues

--- ProjectFiles/main.c
+++ ProjectFiles/main.c
@@ -1,29 +1,67 @@
 #include <FreeRTOS.h>
 #include <task.h>
 #include <stdio.h>
+#include <queue.h>
 #include "pico/stdlib.h"
 
 
-static void led_task(void * data)
+static QueueHandle_t xQueue = NULL;
+
+
+static void led_task(void *pvParameters)
 {
-    (void)data;
+    (void)pvParameters;
     const uint LED_PIN = PICO_DEFAULT_LED_PIN;
+    uint uIValueToSend = 0;
     gpio_init(LED_PIN);
     gpio_set_dir(LED_PIN, GPIO_OUT);
 
     while(1)
     {
         gpio_put(LED_PIN, 1);
+        uIValueToSend = 1;
+        xQueueSend(xQueue, &uIValueToSend, 0U);
         vTaskDelay(100);
+
+        uIValueToSend = 0;
         gpio_put(LED_PIN, 0);
+        xQueueSend(xQueue, &uIValueToSend, 0U);
         vTaskDelay(100);
+    }
+}
+
+static void usb_task(void *pvParameters)
+{
+    (void)pvParameters;
+    uint uIReceivedValue;
+
+    while(1)
+    {
+        xQueueReceive(xQueue, &uIReceivedValue, portMAX_DELAY);
+
+        switch (uIReceivedValue)
+        {
+        case 1:
+            printf("LED is ON!\n");
+            break;
+        case 0:
+            printf("LED is OFF!\n");
+            break;
+        
+        default:
+            break;
+        }
     }
 }
 
 int main(void)
 {
     stdio_init_all();
+
+    xQueue = xQueueCreate(1, sizeof(uint));
+
     xTaskCreate(led_task, "LED_Task", 256, NULL, 1, NULL);
+    xTaskCreate(usb_task, "USB_Task", 256, NULL, 1, NULL);
     vTaskStartScheduler();
 
     while(1) {};
--- README.md
+++ README.md
@@ -3,3 +3,6 @@ This follows a [tutorial by "Learn Embedded Systems"](https://github.com/LearnEm
 
  - Part 1: Blink  
  [Video](https://www.youtube.com/watch?v=jCZxStjzGA8) | [Blog Post](https://learnembeddedsystems.co.uk/freertos-on-rp2040-boards-pi-pico-etc-using-vscode)
+
+ - Part 2: Tasks and Queues  
+ [Video](https://www.youtube.com/watch?v=-fwJZzDpBIg) | [Blog Post](https://learnembeddedsystems.co.uk/freertos-part-2-tasks-and-queues-tutorial)
